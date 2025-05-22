025b5ff339f2e8847123ba7966eedf03fcf50b40
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-05-22 03:01:54 +0200

    Update chromium-snap.sh - use minimal AppRun

--- chromium-snap.sh
+++ chromium-snap.sh
@@ -40,11 +40,6 @@ _create_chromium_appimage() {
 	#!/bin/sh
 	HERE="$(dirname "$(readlink -f "${0}")")"
 	export UNION_PRELOAD="${HERE}"
-	export PATH="${HERE}"/usr/bin/:"${HERE}"/usr/sbin/:"${HERE}"/usr/games/:"${PATH}"
-	export LD_LIBRARY_PATH="${HERE}"/usr/lib/:"${HERE}"/usr/lib/i386-linux-gnu/:"${HERE}"/usr/lib/x86_64-linux-gnu/:"${HERE}"/lib/:"${HERE}"/lib/i386-linux-gnu/:"${HERE}"/lib/x86_64-linux-gnu/:"${LD_LIBRARY_PATH}"
-	export PYTHONPATH="${HERE}"/usr/share/pyshared/:"${HERE}"/usr/lib/python*/:"${PYTHONPATH}"
-	export PYTHONHOME="${HERE}"/usr/:"${HERE}"/usr/lib/python*/
-	export XDG_DATA_DIRS="${HERE}"/usr/share/:"${XDG_DATA_DIRS}"
 	exec ${HERE}/usr/lib/chromium*/chrome "$@"
 	HEREDOC
 	chmod a+x ./"$APP".AppDir/AppRun
